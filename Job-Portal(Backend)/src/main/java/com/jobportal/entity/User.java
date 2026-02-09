a6f008bd256f4c4db766bd875192fd689254bf3a
shweta <shwetachaudhari121103@gmail.com>
2026-02-09 20:49:44 +0530

    Moved complete frontend into Job-Portal-(Frontend) folder

---
+++ Job-Portal(Backend)/src/main/java/com/jobportal/entity/User.java
@@ -0,0 +1,30 @@
+package com.jobportal.entity;
+
+import com.jobportal.dto.AccountType;
+import com.jobportal.dto.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private  String name;
+    //so that email should not dublicate
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+    private AccountType accountType;
+
+    public UserDTO toDTO(){
+        return new UserDTO(this.id,this.name,this.email,this.password,this.accountType);
+    }
+
+}

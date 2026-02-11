a466c5dd9d18e2bf3bfac2c65496fb3ffa860b1a
Shwetac14 <your_email@gmail.com>
2026-02-11 16:07:08 +0530

    Convert frontend from submodule to normal folder

---
+++ Job_Portal_Frontend/src/App.tsx
@@ -0,0 +1,46 @@
+
+import './App.css';
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+import '@mantine/carousel/styles.css';
+import HomePage from './Pages/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FindJobs from './Pages/FindJobs';
+import Header from './Header/header';
+import Footer from './Footer/Footer';
+import { fontFamilyResolver } from '@mantine/core/lib/core/Box/style-props/resolvers/font-family-resolver/font-family-resolver';
+
+function App() {
+  const theme = createTheme({
+    colors: {
+      'bright-sun': [
+        '#fffbeb','#fff3c6','#ffe588','#ffd149','#ffbd20',
+        '#f99b07','#dd7302','#b75006','#943c0c','#7a330d','#461902'
+      ],
+      'mineShaft': [
+        '#f6f6f6','#e7e7e7','#d1d1d1','#b0b0b0','#888888',
+        '#6d6d6d','#5d5d5d','#4f4f4f','#454545','#3d3d3d','#2d2d2d'
+      ]
+    },
+    fontFamily:"poppins,sans-serif"
+  });
+
+  return (
+    <MantineProvider defaultColorScheme='dark' theme={theme}>
+      <BrowserRouter>
+      < Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/find-jobs" element={<FindJobs />} />
+          <Route path="/find-talent" element={<HomePage />} />
+          <Route path="/upload-job" element={<HomePage />} />
+          <Route path="/about" element={<HomePage />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </MantineProvider>
+  );
+}
+
+export default App;
